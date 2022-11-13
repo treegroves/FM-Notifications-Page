9cab0d7ff03c72869f58a79b6f918780d97d1550
tim-greenslade <80554755+treegroves@users.noreply.github.com>
2022-11-13 14:15:13 +1300

    Add first complete solution

--- script.js
+++ script.js
@@ -1,35 +1,23 @@
 const markAllRead = document.querySelector('.mark-read')
 const notificationCard = document.querySelectorAll('.notification-card')
-const dot = document.querySelector('.dot')
-const dots = document.querySelectorAll('.dot')
 const allButtons = document.querySelectorAll('.notification-card')
 let count = (document.querySelector('.notification-count').innerHTML =
   notificationCard.length)
 
 markAllRead.addEventListener('click', () => {
   for (let readNot of notificationCard) {
-    readNot.classList.add('read')
+    readNot.classList.replace('unread', 'read')
   }
   document.querySelector('.notification-count').innerHTML = 0
-  dots.forEach((item) => item.classList.add('dot-read'))
 })
 
-//change status individually
-
 for (let i = 0; i < allButtons.length; i += 1) {
-  allButtons[i].addEventListener('click', read)
-  allButtons[i].addEventListener('click', dotting)
-}
+  allButtons[i].addEventListener('click', read, { once: true })
 
-function read(e) {
-  e.currentTarget.classList.add('read')
-  e.currentTarget.classList.remove('dot')
-  if (count > 0) {
-    document.querySelector('.notification-count').innerHTML = count -= 1
+  function read(e) {
+    e.currentTarget.classList.replace('unread', 'read')
+    if (count > 0) {
+      document.querySelector('.notification-count').innerHTML = count -= 1
+    }
   }
-}
-
-function dotting() {
-  document.querySelector('.dot').innerHTML = ' '
-  console.log('dotting')
 }
